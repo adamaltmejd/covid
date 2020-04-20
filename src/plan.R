cc9d36cd56d9a3dbb667dff2a5e26f615716e618
Adam Altmejd <adam@altmejd.se>
2020-04-20 15:16:17 +0200

    added fixed url img

--- src/plan.R
+++ src/plan.R
@@ -11,6 +11,7 @@ plan <- drake_plan(
 
     lag_plot = target(plot_lagged_deaths(death_dt, death_prediction, my_theme = default_theme)),
     save_plot(lag_plot, file_out(!!file.path("docs", paste0("deaths_lag_sweden_", Sys.Date() , ".png")))),
+    save_plot(lag_plot, file_out(!!file.path("docs", paste0("deaths_lag_sweden_latest.png")))),
     update_web(plots = file_in(!!file.path("docs", paste0("deaths_lag_sweden_", Sys.Date() , ".png"))),
                index = file_out(!!file.path("docs", "index.md")))
 )
